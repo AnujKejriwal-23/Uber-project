b52572280f9cb9a4d08470499eeb0b981f2e6733
AnujKejriwal-23 <22051058@kiit.ac.in>
2025-05-29 16:38:29 +0530

    login and signup page made for both user and captain

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+     <div className='bg-cover bg-auto bg-no-repeat bg-[url(https://img.freepik.com/premium-photo/colorful-flat-vector-illustration-cars-waiting-urban-intersection-sunset_1192310-2399.jpg)] h-screen pt-8 w-full flex justify-between flex-col'>
+      <img className='w-16 ml-8' src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png" alt="" />
+      <div className='bg-white py-5 px-4 pb-7'>
+        <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
+        <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
+      </div>
+     </div>
+    </div>
+  )
+}
+
+export default Home
